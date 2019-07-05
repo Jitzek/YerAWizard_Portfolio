c5a53a072c295413de0f1be90cd6f7e024b84812
Jitze <jitzek@hotmail.com>
2019-07-05 11:43:42 +0200

    yoink

---
+++ Your_a_wizzard_harry/App_Code/dbConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.Data;
+
+/// <summary>
+/// Summary description for dbConfig
+/// </summary>
+public class dbConfig
+{
+    private Database db;
+    public dbConfig()
+    {
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Harry_Potter.mdf;Integrated Security=True";
+        string provider = "System.Data.SqlClient";
+        db = Database.OpenConnectionString(connectionString, provider);
+    }
+
+    public Database Db()
+    {
+        return db;
+    }
+}
